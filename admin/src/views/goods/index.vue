27e0c941be74bfa5f6cb68ab1f8f0d56779d6612
hgx <18618110533@163.com>
2019-09-10 09:02:40 +0800

    合主分支

--- admin/src/views/goods/index.vue
+++ admin/src/views/goods/index.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="app-container">
-    <h1>商品列表</h1>
+    名称：
+    <el-button> 新增</el-button> 
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
